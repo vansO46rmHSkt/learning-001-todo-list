37559ffe11e1c482e129f972571c5d59f25d8011
vansO46rmHSkt <96056341+vansO46rmHSkt@users.noreply.github.com>
2023-11-03 10:52:32 +0900

    create github actions to build and deploy

--- aws/create-base-stack.sh
+++ aws/create-base-stack.sh
@@ -6,7 +6,10 @@ aws cloudformation create-stack \
     --stack-name $BASE_STACK_NAME \
     --template-body file://base-stack.json \
     --capabilities CAPABILITY_NAMED_IAM \
-    --parameters ParameterKey=UserArnForDeloyS3,ParameterValue=$USER_ARN_FOR_DELOY_S3 ParameterKey=LocalIpAddress,ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32 \
+    --parameters \
+        ParameterKey=GithubUserName,ParameterValue=$GITHUB_USER_NAME \
+        ParameterKey=GithubRepositoryName,ParameterValue=$GITHUB_REPOSITORY_NAME \
+        ParameterKey=LocalIpAddress,ParameterValue=$(curl -s http://checkip.amazonaws.com/)/32 \
     --role-arn $ROLE_ARN \
     --on-failure DELETE \
     --no-retain-except-on-create
