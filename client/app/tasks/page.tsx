9492b01bd47c76a3fcbfdb38f9d07691503368de
vansO46rmHSkt <96056341+vansO46rmHSkt@users.noreply.github.com>
2023-11-05 16:22:49 +0900

    tempolary fix to call api from route handler

--- client/app/tasks/page.tsx
+++ client/app/tasks/page.tsx
@@ -1,11 +1,25 @@
 import { Task } from "@/types/tasks";
-import { headers } from "next/headers";
 import TodoList from "./TodoList";
+import { sleep } from "@/lib/misc";
+
+const TASKS = [
+  {
+    id: "1",
+    title: "title-1",
+  },
+  {
+    id: "2",
+    title: "title-2",
+  },
+  {
+    id: "3",
+    title: "title-3",
+  },
+] as const satisfies readonly Task[];
 
 const TodoListPage = async () => {
-  const tasks = (await fetch(
-    `http://${headers().get("host")}/api/v1/tasks`,
-  ).then((data) => data.json())) as Task[];
+  const tasks = await sleep(1000).then(() => [...TASKS]);
+
   return <TodoList tasks={tasks} />;
 };
 
