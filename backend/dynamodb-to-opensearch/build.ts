412397bc75d4cb8cf624546f876157db910a4734
vansO46rmHSkt <96056341+vansO46rmHSkt@users.noreply.github.com>
2023-11-28 08:21:03 +0900

    Use esm format in esbuild

--- backend/dynamodb-to-opensearch/build.ts
+++ backend/dynamodb-to-opensearch/build.ts
@@ -5,9 +5,13 @@ esbuild
     entryPoints: ["./src/index.ts"],
     bundle: true,
     minify: true,
-    outfile: "./dist/index.js",
+    outfile: "./dist/index.mjs",
     platform: "node",
     target: "node18",
+    format: "esm",
+    banner: {
+      js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+    },
   })
   .catch((err) => {
     process.stderr.write(err.stderr);
